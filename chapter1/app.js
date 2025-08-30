fe41bd8642d2f2b454996bddd72e0bcbfe93cd1c
waniawaseem <waniawaseem001@gmail.com>
2025-08-31 01:58:27 +0500

    done

---
+++ chapter1/app.js
@@ -0,0 +1,12 @@
+//question#2
+// alert("Error! Please enter valid password");
+
+//question#3
+//alert("Welcome to Js Land ... \n Happy Coding!");
+
+//question#4
+//alert("Welcome to Js Land ...");
+// alert("Happy Coding!")
+
+//question#5
+//alert("Hello.. I can run JS through my web browser's console")
