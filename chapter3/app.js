6ad0d5291dbd829a85a27f3bd3f4fa181c9b5f8f
waniawaseem <waniawaseem001@gmail.com>
2025-09-08 22:28:32 +0500

    done

---
+++ chapter3/app.js
@@ -0,0 +1,17 @@
+//question#1
+//let age = 17;
+//alert("My age is "+age);
+
+//question#2
+//let visit= 15;
+//alert("You have visited this site "+visit+ " times")
+
+//question#3
+//let birthdayyear;
+//birthdayyear=2008;
+//document.writeln("My birthday year is "+birthdayyear)
+//question#4
+//let visitorName = "John Doe";
+    //let productTitle = "T-shirt";
+    //let quantity = 5;
+    //document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
---
+++ chapter4/app.js
@@ -0,0 +1,13 @@
+ // 1. Declare 3 variables in one statement
+    //let name, age, city;
+
+    // 2. Legal & Illegal variable names
+    // ✅ Legal variable names
+    //let myName, _age, $dollar, user123, first_name;
+    
+    // ❌ Illegal variable names (these will cause errors if uncommented)
+    // let 123name;
+    // let my-name;
+    // let var;
+    // let user name;
+    // let @hello;
