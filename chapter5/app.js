bb36c3f3aee5f9024f4cced9d0e448929db24b1c
waniawaseem <waniawaseem001@gmail.com>
2025-09-10 22:48:06 +0500

    done

---
+++ chapter5/app.js
@@ -0,0 +1,63 @@
+//question#1
+//let num1=3
+//let num2=5
+//addition
+//document.write(`sum of ${num1} and ${num2} is ${num1+num2}`)<br>
+
+//question#2
+//subraction
+//document.write(`sum of ${num1} and ${num2} is ${num1-num2}`)<br>
+
+//multiplication
+//document.write(`sum of ${num1} and ${num2} is ${num1*num2}`)
+
+//modulus
+//document.write(`sum of ${num1} and ${num2} is ${num1%num2}`)
+
+//division
+//document.write(`sum of ${num1} and ${num2} is ${num1/num2}`)
+
+//question#3
+// a. Declare a variable
+//let num;
+
+// b. Show the value of variable
+//document.write(`Value after variable declaration is: ${num} <br>`);
+
+// c. Initialize the variable with some number
+//num = 5;
+
+// d. Show the initial value
+//document.write(`Initial value: ${num} <br>`);
+
+// e. Increment the variable
+//num++;
+
+// f. Show the value after increment
+//document.write(`Value after increment is: ${num} <br>`);
+
+// g. Add 7 to the variable
+//num = num + 7;
+
+// h. Show the value after addition
+//document.write(`Value after addition is: ${num} <br>`);
+
+// i. Decrement the variable
+//num--;
+
+// j. Show the value after decrement
+//document.write(`Value after decrement is: ${num} <br>`);
+
+// k. Show the remainder after dividing the variable by 3
+//let remainder = num % 3;
+
+// l. Output the remainder
+//document.write(`The remainder is : ${remainder}`);
+
+//question#4
+//let ticketPrice = 600;
+//let numberOfTickets = 5;
+//let totalCost = ticketPrice * numberOfTickets;
+
+//document.write(`Cost of one movie ticket is ${ticketPrice} PKR<br>`);
+//document.write(`The cost of buying ${numberOfTickets} tickets is ${totalCost} PKR`);  
