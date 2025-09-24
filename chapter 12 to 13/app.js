07cdd531af3ad09e7e1af9928eb44b2ec797f39e
waniawaseem <waniawaseem001@gmail.com>
2025-09-24 23:36:37 +0500

    done

---
+++ chapter 12 to 13/app.js
@@ -0,0 +1,56 @@
+// question # 1
+// question # 2
+// let num1= +prompt(`Enter first integer:`)
+// let num2= +prompt(`Enter second integer:`)
+// if (num1>num2){
+//      alert("The larger number is: " + num1);
+// }
+// else if (num2 > num1) {
+//       alert("The larger number is: " + num2);
+//     } 
+//     else {
+//       alert("Both numbers are equal");
+//     }
+// question # 3
+// let num= +prompt(`Enter the Number`)
+// if (num > 0) {
+//       alert("The number is Positive");
+//     } 
+//     else if (num < 0) {
+//       alert("The number is Negative");
+//     } 
+//     else {
+//       alert("The number is Zero");
+//     }
+// question # 4
+// question # 5
+    // var correctPassword = "abc123";
+
+    // // (b) User se password input lena
+    // var userPassword = prompt("Enter your password:");
+
+    // // (c) Validate karna
+    // if (!userPassword) {
+    //   // (i) Agar user ne password enter hi nahi kiya
+    //   alert("Please enter your password");
+    // } 
+    // else if (userPassword === correctPassword) {
+    //   // (ii) Agar dono passwords same hain
+    //   alert("Correct! The password you entered matches the original password");
+    // } 
+    // else {
+    //   // Agar password galat hai
+    //   alert("Incorrect password");
+    // }
+// question # 6
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+//console.log(greeting);
+// question # 7
