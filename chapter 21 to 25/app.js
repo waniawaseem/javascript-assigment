d24ff1a01f3322d3f774cb26c2113163e29165e3
waniawaseem <waniawaseem001@gmail.com>
2025-10-14 01:28:03 +0500

    done

---
+++ chapter 21 to 25/app.js
@@ -0,0 +1,87 @@
+//question # 1
+// // Take first name input from user
+// let firstName=prompt(`Enter your first name:`)
+
+// // Take last name input from user
+// let lastName=prompt(`Enter your last name:`)
+
+// // Merge both names into a new variable
+// let fullName=(`${firstName} ${lastName}`)
+
+// // Greet the user using full name
+// alert(`Hello, ${fullName}! Welcome!`)
+
+//question # 2
+// // Take user input about favorite mobile phone model
+// let favoritePhone = prompt(`Enter your favorite mobile phone model:`);
+
+// // Display the input and its length in browser
+// document.write("My favorite phone is: " + favoritePhone + "<br>");
+// document.write(`Length of string: ${favoritePhone.length}`);
+
+//question # 3
+// // Store the word in a variable
+// let word = "Hello World";
+
+// // Find the last index of letter "l"
+// let lastIndex = word.lastIndexOf("l");
+
+// // Display the result in browser
+// document.write("String: " + word + "<br>");
+// document.write("Last index of 'l': " + lastIndex);
+
+//question # 4
+// // Store the word in a variable
+// var word = "Pakistani";
+
+// // Find the character at 3rd index
+// var charAt3 = word.charAt(3);
+
+// // Display the result in browser
+// document.write("String: " + word + "<br>");
+// document.write("Character at index 3: " + charAt3);
+
+//question # 5
+// // Take first name input from user
+// var firstName = prompt("Enter your first name:");
+
+// // Take last name input from user
+// var lastName = prompt("Enter your last name:");
+
+// // Merge both names using concat() method
+// var fullName = firstName.concat(" ", lastName);
+
+// // Greet the user using full name
+// document.write("Hello, " + fullName + "! Welcome!");
+
+//question # 6
+// Take first name input from user
+// var firstName = prompt("Enter your first name:");
+
+// // Take last name input from user
+// var lastName = prompt("Enter your last name:");
+
+// // Merge both names using concat() method
+// var fullName = firstName.concat(" ", lastName);
+
+// // Greet the user using full name
+// document.write("Hello, " + fullName + "! Welcome!");
+
+//question # 7
+// // Store the word in a variable
+// var city = "Hyderabad";
+
+// // Replace "Hyder" with "Islam"
+// var newCity = city.replace("Hyder", "Islam");
+
+// // Display the result in browser
+// document.write("City: " + city + "<br>");
+// document.write("After replacement: " + newCity);
+
+//question # 8
+
+//question # 9
+//question # 10
+//question # 11
+//question # 12
+//question # 13
---
+++ chapter 26 to 30/app.js
@@ -0,0 +1,30 @@
+//question # 1
+// // Take a positive integer input from user
+// var num = +prompt("Enter a positive integer:");
+
+// // Display results in browser
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+//question # 2
+// // Take a negative floating-point number input from user
+// let num = +prompt("Enter a negative floating point number:");
+
+// // Display results in browser
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+//question # 3
+// // Take a number input from user
+// let num = +prompt("Enter a number:");
+
+// // Find the absolute value
+// let absoluteValue = Math.abs(num);
+
+// // Display the result in browser
+// document.write("The absolute value of " + num + " is " + absoluteValue);
+
