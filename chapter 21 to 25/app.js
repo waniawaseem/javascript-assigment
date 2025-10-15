5c95a183870c90948fcb6ae306ce13dc1c83d0ad
waniawaseem <waniawaseem001@gmail.com>
2025-10-16 01:58:57 +0500

    done

--- chapter 21 to 25/app.js
+++ chapter 21 to 25/app.js
@@ -79,9 +79,136 @@
 // document.write("After replacement: " + newCity);
 
 //question # 8
+// let message = (`Ali and Sami are best friends. They play cricket and football together.`)
+// // Replace all occurrences of "and" with "&"
+// let newMessage =message.replace(/and/g,"&")
+// document.write(newMessage)
 
 //question # 9
+// var str ="472"        // string value
+// document.write(`Value: ${str}<br/>`)
+// document.write(`Type: ${typeof(str)}<br/>`)
+
+// // Convert string to number
+// var num = parseInt(str)      // or you can use Number(str)
+// document.write(`Value: ${num}<br/>`)
+// document.write(`Type: ${typeof(num)}<br/>`)
+
 //question # 10
+// let userInput = prompt(`Enter any Text`);   // take input from user
+// let capitalText = userInput.toUpperCase();  // convert to capital letters
+// document.write(`User Input: ${userInput} <br/>`);
+// document.write(`Upper Case: ${capitalText}`);
+
 //question # 11
+// var userInput=prompt('Please enter any word');
+// var firstChar=userInput.slice(0,1);
+// firstChar=firstChar.toUpperCase();
+// var otherChars=userInput.slice(1);
+// otherChars=otherChars.toLowerCase();
+// var titleCase=firstChar+otherChars;
+// document.write(`User Input :${userInput}<br>Title case :${titleCase} `);
+
 //question # 12
+// var num = 35.36;
+
+// // Convert number to string
+// var str = num.toString();
+
+// // Remove the dot
+// var result = str.replace(".", "");
+
+// // Display the result
+// document.write("Number: " + num + "<br>");
+// document.write("Result: " + result); 
+
 //question # 13
+// var userInput=prompt('Enter your name: ');
+// var charCode;
+// for(var i=0;i<userInput.length;i++){
+//     charCode=userInput.charCodeAt(i);
+//      if(charCode===33||charCode===44||charCode===46||charCode===64){
+//         alert("Please enter a valid username")
+//      }
+// }
+
+//question # 14
+// var items = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+// userInput = userInput.toLowerCase(); // convert input to lowercase
+
+// var index = items.indexOf(userInput); // find index of the item
+
+// if (index !== -1) {
+//   document.write(userInput + " is <b>available</b> at index " + index + " in our bakery");
+// } else {
+//   document.write("We are sorry. " + userInput + " is <b>not available</b> in our bakery");
+// }
+
+//question # 15
+// var password = prompt("Enter your password: ");
+// var passwordLength = false;
+// var passwordChar = false;
+// var passwordInt = false;
+// var passwordStart = true;
+
+// //Checking for Alphabet
+// for (i = 0; i < password.length; i++){
+//     var charValue = password[i].charCodeAt(0);
+
+//     //Checking for UpperCase Letters
+//     if (charValue >= 65 && charValue <= 90 ){
+//         passwordChar = true;
+//     }
+
+//     //Checking for LowerCase Letters
+//     else if (charValue >= 97 && charValue <= 122 ){
+//         passwordChar = true;
+//     }
+// }
+
+// //Checking for Numbers
+// for (i = 0; i < password.length; i++){
+//     var charValue = password[i].charCodeAt(0);  
+//     if (charValue >= 48 && charValue <= 57 ){
+//         passwordInt = true;
+//     }
+// }
+
+// //Checking for first letter
+// var charValue = password.charCodeAt(0);
+// if (charValue >= 48 && charValue <= 57 ){
+//     passwordStart = false;
+// }
+
+// //Checking for Length
+// if(password.length >= 6){
+//     passwordLength = true;
+// }
+
+// if(passwordChar === false || passwordInt === false || passwordLength === false || passwordStart === false){
+//     alert("Enter valid Password");
+// }
+
+// else{
+//     alert("Password Approved");
+// }
+
+//question # 16
+// var str='University of Karachi';
+// var arr =str.split("");
+// for(var i=0;i<str.length;i++){
+//     document.write(arr[i]+'<br>')
+// }
+
+//question # 17
+// var userInput=prompt('Write any word');
+// document.write('User input :'+userInput+'<br>'+'Last character of input:'+userInput[userInput.length-1])
+
+//question # 18
+// var str='The quick brown fox jumps over the lazy dog';
+// str =str.toLowerCase();
+// var count=str.match(/the/g);
+// var length=count.length;
+// document.write('Text:'+str+'<br>'+'There are '+length+' occurence(s) of word "the"')
--- chapter 26 to 30/app.js
+++ chapter 26 to 30/app.js
@@ -28,3 +28,55 @@
 // // Display the result in browser
 // document.write("The absolute value of " + num + " is " + absoluteValue);
 
+//question # 4
+// Generate a random number between 1 and 6
+    // var diceValue = Math.floor(Math.random() * 6) + 1;
+
+    // // Display the result in the browser
+    // document.write("Random dice value: " + diceValue);
+
+//question # 5
+// let user1=prompt(`Enter 1st Player Name`);
+// let user2=prompt(`Enter 2st Player Name`);
+// let guess1=prompt(`Select any one Head or Tail?`);
+// let guess2=prompt(`Select any one Head or Tail?`);
+// let random=Math.round(Math.random());
+// if (random===1 && guess1==="head"){
+//     document.write("🎉 Congratulations! 1st Player Win");
+// }
+// else if (random===0 && guess1==="tail"){
+//      document.write("🎉 Congratulations! 1st Player Win");
+// }
+// else {
+//      document.write("🎉 Congratulations! 2nd Player Win");
+// }
+//question # 6
+// Generate a random number between 1 and 100
+    // var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    // // Display the result in the browser
+    // document.write("Random number between 1 and 100: " + randomNumber);
+
+//question # 7
+// // Ask the user for their weight
+//     var userInput = prompt("Enter your weight :");
+
+//     // Extract numeric value from the input using parseFloat()
+//     var weight = parseFloat(userInput);
+
+//     // Display the result in the browser
+//     document.write("The weight of user is " + weight + " kilograms");
+
+//question # 8
+//  // Generate a random secret number between 1 and 10
+//     var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+//     // Ask the user for their guess
+//     var userGuess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+
+//     // Check if the guess is correct
+//     if (userGuess === secretNumber) {
+//       document.write("🎉 Congratulations! You guessed the secret number " + secretNumber + " correctly!");
+//     } else {
+//       document.write("❌ Sorry, the secret number was " + secretNumber + ". Try again!");
+//     }
